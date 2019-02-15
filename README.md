dd2d4a31d58c51a000ba894b64facebb53b38a20
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-02-15 01:20:13 +0100

    Added projec to GitHub

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # bptf-quick-stats
+
 A faster way to open stats pages for bptf items
+
+## Requirements
+
+* [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) (Chrome)
+* [Greasemonkey](https://addons.mozilla.org/en-us/firefox/addon/greasemonkey/) (Firefox)
+
+## Installation
+
+1. Ensure that you have either Tampermonkey or Greasemonkey installed.
+2. Download the script [`scanner.user.js`](https://github.com/Nicklason/bptf-quick-stats/blob/master/stats.user.js?raw=true).
+3. Allow popup windows from backpack.tf.
+4. The script should now be installed and ready to use.
+
+## Usage
+
+Hover over an item and the `shift` key to open the stats page.
---
+++ status.user.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         BackpackTF Quick Stats
+// @namespace    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+// @version      1.0.0
+// @description  A faster way to open stats pages for bptf items
+// @author       Nicklason
+// @match        https://backpack.tf/profiles/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// ==/UserScript==
+
+const $ = window.jQuery;
+
+(function () {
+    'use strict';
+
+    // Wait for the inventory to load
+    waitInventory();
+
+    // Listen for inventory refresh
+    $('#refresh-inventory').click(function () {
+        waitInventory();
+    });
+})();
+
+function waitInventory (check = false) {
+    $('.item').not('.spacer').off('click');
+    if (check) {
+        const pages = $('#backpack').children('.backpack-page');
+        if (pages.length != 0) {
+            inventoryLoaded();
+            return;
+        }
+    }
+
+    setTimeout(function () {
+        waitInventory(true);
+    }, 1000);
+}
+
+function inventoryLoaded () {
+    $('.item').not('.spacer').on('mouseover', function () {
+        const self = this;
+        $(document).bind('keydown', function (e) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (e.which == 16) {
+                const url = constructLink($(self));
+                window.open(url, '_blank');
+            }
+        });
+    }).on('mouseout', function () {
+        $(document).unbind('keydown');
+    });
+}
+
+function constructLink (element) {
+    const name = element.data('name');
+    const quality = element.data('q_name');
+    const craftable = element.data('craftable') == 1;
+    const tradeable = element.data('tradable') == 1;
+    const effect = element.data('effect_id') ? element.data('effect_id') : null;
+
+    const url = `https://backpack.tf/stats/${quality}/${name}/${tradeable ? 'Tradeable' : 'Non-Tradable'}/${craftable ? 'Craftable' : 'Non-Craftable'}${effect === null ? '' : `/${effect}`}`;
+    return url;
+}
